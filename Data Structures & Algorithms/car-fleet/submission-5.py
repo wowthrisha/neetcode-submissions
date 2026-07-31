fc24c70bb367dadf3ebf8834ea9b171bcb2d2f50
wowthrisha <136899011+wowthrisha@users.noreply.github.com>
2026-07-31 23:32:41 +0530

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n=len(position)
+        #t=[0]*n
+        ps=list(zip(position,speed))
+        ps.sort(key = lambda x:x[0],reverse=True)
+
+        if len(position)>=1 and len(speed)>=1:
+            f=1
+        else:
+            f=0
+    
+        pft=0
+        for p,s in ps:
+            t=(target-p)/s 
+            if p==ps[0][0]:
+                pft=t
+            if pft<t:
+                f+=1
+                pft=t
+            
+   
+        
+        return f
+        
